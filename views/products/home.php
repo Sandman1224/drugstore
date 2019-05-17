142f5382d0bda389da2cfd5a432d966d6260e0f8
Martin Ganami <martinganami@gmail.com>
2019-05-16 21:44:43 -0300

    Integracion de búsqueda de datos y grilla con funciones adicionales

--- views/products/home.php
+++ views/products/home.php
@@ -1,5 +1,6 @@
 <?php
 use yii\helpers\Html;
+use yii\grid\GridView;
 
 $this->title = 'Productos';
 $this->params['breadcrumbs'][] = $this->title;
@@ -7,4 +8,35 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="containter">
     <h1><?= Html::encode($this->title) ?> </h1>
+
+    <?= Html::a('<span class="glyphicon glyphicon-plus"></span>', ['create'], ['class' => 'btn btn-primary']) ?>
+
+    <?= 
+    GridView::widget([
+        'dataProvider' => $dataProducts,
+        'emptyText' => '¡No se encontraron productos',
+        'columns' => [
+            [
+                'attribute' => '_id',
+                'visible' => false
+            ],
+            [
+                'attribute' => 'name',
+                'label' => 'Nombre'
+            ],
+            [
+                'attribute' => 'price',
+                'label' => 'Precio'
+            ],
+            [
+                'attribute' => 'description',
+                'label' => 'Descripción'
+            ],
+            [
+                'attribute' => 'quantity',
+                'label' => 'Cantidad'
+            ]
+        ]
+    ]);
+    ?>
 </div>
